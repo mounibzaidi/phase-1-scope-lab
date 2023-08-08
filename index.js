aed4f67629714f291f4a7d1f7508947a6fa35b9e
zaidi mounib <mounibzaidi@gmail.com>
2023-08-08 10:19:13 +0100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = 'john'
+var bestCustomer;
+function upperCaseCustomerName() {
+    if (typeof customerName === 'string') {
+        return customerName = customerName.toUpperCase();
+    }
+}
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'emily'
+}
